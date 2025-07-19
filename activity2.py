db2eb424289e38d960b040cac8df63be6dda8258
shivaansh999 <sugandh.jolly@wonderland.edu.in>
2025-07-19 17:22:35 +0530

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,17 @@
+a=5
+b=4.8
+c=True
+d='shivaansh'
+print("data type of a ",type (a))
+print("data type of b ",type (b))
+print("data type of c ",type (c))
+print("data type of d ",type (d))
+a=str(a)
+print("data type of a ",type (a))
+fname=input('enter your first name')
+print('your name is ',fname)
+print("first character ",fname[0])
+print("last character",fname[-1])
+print("length",len(fname))
+lname=input('enter your last name')
+print(fname+lname)
