780fc4a468199f5ae0f9bb73c3d1966d7537146e
shivaansh999 <sugandh.jolly@wonderland.edu.in>
2025-07-21 15:45:18 +0530

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,8 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=96
+tree5=11
+add=tree1+tree2+tree3+tree4+tree5
+sum=add/5
+print(sum)
---
+++ activity4.py
@@ -0,0 +1,7 @@
+maths=40
+hindi=50
+english=60
+science=70
+sum=maths+hindi+english+science
+total=(sum/400)*100
+print("rajs score ",total)
